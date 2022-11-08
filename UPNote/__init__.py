1efd1b2adf116bc6df9882025d304a59a855d7bd
HedyLuo <1570968754@qq.com>
2022-11-08 20:19:13 +0800

    python基础知识

---
+++ UPNote/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author: hedy
+@Date: 2022/11/08/19:38 
+@Description: 
+"""
---
+++ UPNote/file_op.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author: hedy
+@Date: 2022/11/08/20:17 
+@Description: 
+"""
+
