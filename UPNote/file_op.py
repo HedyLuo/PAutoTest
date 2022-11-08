6ac569308baa3d09938efc61896cf4d3dbf3e6ba
HedyLuo <1570968754@qq.com>
2022-11-08 20:42:55 +0800

    python基础知识

--- UPNote/file_op.py
+++ UPNote/file_op.py
@@ -6,3 +6,16 @@
 @Description: 
 """
 
+# 写入文件
+file = open("name.txt", 'w')
+file.write("诸葛亮")
+file.close()
+
+# 读取文件
+file1 = open('name.txt')
+print(file1.read())
+file1.close()
+
+file3 = open('name.txt','a')
+file3.write('刘备')
+file3.close()
